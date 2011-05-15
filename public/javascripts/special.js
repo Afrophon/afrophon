ead69c15e5e0bd42775cde08368b1d62952b6807
Lucas Waller <lcstwllr@gmail.com>
2011-05-15 13:48:03 +0200

    afrophon

--- public/javascripts/special.js
+++ public/javascripts/special.js
@@ -1,14 +1,15 @@
-$(function() {
-	$('a.event').bind('click', function(event) {
+$(function() {	
+		alert('t');
+	var links = $('.event');
+	links.unbind('click');
+	links.bind('click', function(event) {
 		var element = $(this);
-		alert(element.attr('href'));
 		//history.pushState(state, "Afrophon", $(this).attr('href'));
 		$.get("ajax"+element.attr('href'), function(data){ 
 			element.css({'z-index':15, position:'absolute'});
 			element.animate({width: 969, height: 969, left:0; top:0}, 1000);
 			//$('.dyncontent').html(data);
 		});
-		event.preventDefault();
 		return false;
 	});
 });
