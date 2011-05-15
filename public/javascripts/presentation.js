eb5a15934091c861645750c7c47f624e495c3aa5
Lucas Waller <lcstwllr@gmail.com>
2011-05-15 16:20:57 +0200

    afrophon

--- public/javascripts/presentation.js
+++ public/javascripts/presentation.js
@@ -1,3 +1,24 @@
 $(function() {	
+	var pres = $('#pages');
+	var slidenr = 0;
+	var slidecount = pres.children().size() - 1;
 	
+	$('#control-left').click(function () {
+		slidenr--;
+		if (slidenr >= 0)
+			pres.animate({'left': -874 * slidenr}, 1000);
+		else {
+			slidenr = slidecount;
+			pres.animate({'left': -874 * slidenr}, 1000);
+		}
+	});
+	$('#control-right').click(function () {
+		slidenr++;
+		if (slidenr <= slidecount)
+			pres.animate({'left': -874 * slidenr}, 1000);
+		else {
+			slidenr = 0;
+			pres.animate({'left': 0}, 1000);
+		}
+	});
 });
