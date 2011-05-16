b612d66dee2533bb5fe943345812f9e9597218a2
Lucas Waller <lcstwllr@gmail.com>
2011-05-16 15:34:13 +0200

    afrophon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,14 +11,16 @@ def ajaxindex
 end
 def kontakt
   begin
-	Pony.mail(:from => params[:email], :to => 'lcstwllr@gmail.com', :via => :smtp, :via_options => {
-   		:address        => "smtp.sendgrid.net",
-  		:port           => "25",
-  		:authentication => :plain,
-  		:user_name      => ENV['SENDGRID_USERNAME'],
-  		:password       => ENV['SENDGRID_PASSWORD'],
-  		:domain         => ENV['SENDGRID_DOMAIN']
-  	})
+    if(params[:email] != "") 
+		Pony.mail(:from => params[:email], :to => 'lcstwllr@gmail.com', :via => :smtp, :via_options => {
+   			:address        => "smtp.sendgrid.net",
+  			:port           => "25",
+  			:authentication => :plain,
+  			:user_name      => ENV['SENDGRID_USERNAME'],
+  			:password       => ENV['SENDGRID_PASSWORD'],
+  			:domain         => ENV['SENDGRID_DOMAIN']
+  		})
+  	end
   rescue
   end
 end
