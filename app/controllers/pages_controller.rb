a4b0214ef577a44366692794a557424c6aaf451d
Lucas Waller <lcstwllr@gmail.com>
2011-05-16 15:24:19 +0200

    afrophon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,14 @@ end
 def ajaxindex
 	render 'index.html.erb', :layout => nil
 end
+def kontakt
+	Pony.mail(:to => 'lcstwllr@gmail.com', :via => :smtp, :via_options => {
+   		:address        => "smtp.sendgrid.net",
+  		:port           => "25",
+  		:authentication => :plain,
+  		:user_name      => ENV['SENDGRID_USERNAME'],
+  		:password       => ENV['SENDGRID_PASSWORD'],
+  		:domain         => ENV['SENDGRID_DOMAIN']
+  	})
+end
 end
