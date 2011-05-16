b93cc5df3cecb6daa9b69e2c77eddb509b154bb1
Lucas Waller <lcstwllr@gmail.com>
2011-05-16 20:08:36 +0200

    afrophon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,16 +10,25 @@ def ajaxindex
 	render 'index.html.erb', :layout => nil
 end
 def kontakt
-  begin
-	@kontakt = Kontakt.new
-	@kontakt.email = params[:email]
-	@kontakt.name = params[:name]
-	@kontakt.telefon = params[:telefon]
-	@kontakt.nachricht = params[:nachricht]
-	@kontakt.sendmail
-  	render 'success.html.erb'
-  rescue
-  	render 'error.html.erb'
-  end
+	@nachricht = ""
+	if params[:authenticity_token] == form_authenticity_token
+		@kontakt = Kontakt.new
+		@kontakt.email = params[:email]
+		@kontakt.name = params[:name]
+		@kontakt.telefon = params[:telefon]
+		@kontakt.nachricht = params[:nachricht]
+		@kontakt.sendmail
+		@nachricht = "Wir haben deine Nachricht bekommen und werden uns in Kürze bei dir melden. Vielen Dank!"
+		@kontakt.errors.full_messages.each do |error|
+    		if (error == "Email can't be blank")
+    	    	@nachricht = "Gib bitte eine E-Mail Adresse an!"
+    	    	break
+    		elsif (error == "Email is invalid")
+     		   	@nachricht = "Die eingegebene E-Mail Adresse existiert nicht!"
+     		   	break
+    		end
+    	end
+  	end
+  	render 'kontakt.html.erb', :locals => {:nachricht => @nachricht}
 end
 end
--- app/models/kontakt.rb
+++ app/models/kontakt.rb
@@ -5,7 +5,6 @@ class Kontakt
   
   attr_accessor :name, :email, :telefon, :nachricht
   
-  validates_presence_of :name
   validates_presence_of :email
   validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\z/
   
