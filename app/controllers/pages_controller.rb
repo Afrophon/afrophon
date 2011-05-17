7ff64cfcda5189f57e0cc54aee7e7612d064c5bd
Lucas Waller <lcstwllr@gmail.com>
2011-05-17 15:03:27 +0200

    afrophon

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,6 +29,26 @@ def kontakt
     		end
     	end
   	end
-  	render 'kontakt.html.erb', :locals => {:nachricht => @nachricht}
+  	render 'kontakt.html.erb', :locals => {:nachricht => @nachricht, :email => params[:email], :name => params[:name], :telefon => params[:telefon], :text => params[:nachricht]}
+end
+def newsletter
+	@nachricht = ""
+	if params[:authenticity_token] == form_authenticity_token
+		@kontakt = Kontakt.new
+		@kontakt.email = params[:email]
+		@kontakt.name = params[:name]
+		@kontakt.sendmail
+		@nachricht = "Wir haben deine Nachricht bekommen <br /> und werden uns in Kürze bei dir melden. Vielen Dank!"
+		@kontakt.errors.full_messages.each do |error|
+    		if (error == "Email can't be blank")
+    	    	@nachricht = "Gib bitte eine E-Mail Adresse an!"
+    	    	break
+    		elsif (error == "Email is invalid")
+     		   	@nachricht = "Die eingegebene E-Mail Adresse existiert nicht!"
+     		   	break
+    		end
+    	end
+  	end
+  	render 'newsletter.html.erb', :locals => {:nachricht => @nachricht, :email => params[:email], :name => params[:name]}
 end
 end
---
+++ app/models/newsletter.rb
@@ -0,0 +1,27 @@
+class Newsletter
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  extend ActiveModel::Naming
+  
+  attr_accessor :name, :email
+  
+  validates_presence_of :email
+  validates_format_of :email, :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\z/
+  
+  def sendmail
+  	if self.valid?
+		Pony.mail(:from => self.email, :to => 'afrophon@gmail.com', :subject => 'Newsletter-Anmeldung', :html_body => 'Name: ' + self.name + '<br>Email: ' + self.email, :via => :smtp, :via_options => {
+   			:address        => "smtp.sendgrid.net",
+  			:port           => "25",
+  			:authentication => :plain,
+  			:user_name      => ENV['SENDGRID_USERNAME'],
+  			:password       => ENV['SENDGRID_PASSWORD'],
+  			:domain         => ENV['SENDGRID_DOMAIN']
+  		})
+  	end
+  end
+  
+  def persisted?
+    false
+  end
+end
