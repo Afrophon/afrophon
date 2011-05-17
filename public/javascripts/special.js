6f734d6e8bcfaa0b1c515d6206cb999dc9d266ee
Lucas Waller <lcstwllr@gmail.com>
2011-05-17 12:52:57 +0200

    afrophon

--- public/javascripts/special.js
+++ public/javascripts/special.js
@@ -9,13 +9,13 @@ $(function() {
 			document.location.href=$(this).attr('href');
 		else {
 			var element = $(this);
-			$('.dyncontent').append('<div id="ovf"><div id="animation"></div></div>');
-			var animation = $('#animation');
-			animation.offset(element.offset()).width(285).height(285).css('display', 'none');
+			$('.dyncontent').append('<div id="ovf"></div>');
+			var ovf = $('#ovf');
+			ovf.offset(element.offset()).width(285).height(285);
 			$.get("ajax/event/nil"+element.attr('href'), function(data){ 
 				history.pushState(state, "Afrophon", element.attr('href'));
-				animation.css({'z-index':15, display: 'block'}).html(data);
-				animation.animate({width: 969, height: 669, left:0, top:0}, 1000, function () {
+				ovf.html(data).children().css({'z-index':15, display: 'block', left:0, top:0});
+				ovf.animate({width: 969, height: 669, left:118, top:402}, 1000, function () {
 					$('#events').remove();
 				});
 			});
