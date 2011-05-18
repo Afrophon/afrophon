d86a74cb2e83c110858e3c86d1415cf084f778a5
Lucas Waller <lcstwllr@gmail.com>
2011-05-18 23:39:19 +0200

    pan2

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 document.ready = function() {
-	var pageheights = {'/' : 910, '/ueber-uns.html' : 1010,  '/events.html' : 1100,  '/unterstuetzer.html' : 850,  '/kontakt.html' : 1000,  '/impressum.html' : 1000,  '/libellaswing.html' : 1100,  '/datenschutz.html' : 1250,  '/newsletter.html' : 1000,  '/findusmc.html' : 1100,  '/meissentanzt.html' : 1100,  '/sponsoren.html' : 1000, '/indierocknight.html' : 1100, '/investorrelations.html' : 1050, '/bands.html' : 1100, '/anteilseigner.html' : 1100};
+	var pageheights = {'/' : 810, '/ueber-uns.html' : 1010,  '/events.html' : 1100,  '/unterstuetzer.html' : 810,  '/kontakt.html' : 1000,  '/impressum.html' : 1000,  '/libellaswing.html' : 1100,  '/datenschutz.html' : 1250,  '/newsletter.html' : 1000,  '/findusmc.html' : 1100,  '/meissentanzt.html' : 1100,  '/sponsoren.html' : 1000, '/indierocknight.html' : 1100, '/investorrelations.html' : 1050, '/bands.html' : 850, '/anteilseigner.html' : 1020};
 	var events = ['/findusmc.html','/libellaswing.html','/indierocknight.html','/meissentanzt.html'];
 
 	var state = {};
