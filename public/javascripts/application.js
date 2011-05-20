1ea8f8d16ab4edad72dc3753c5f1a08a7611c41d
Lucas Waller <lcstwllr@gmail.com>
2011-05-20 22:16:25 +0200

    fixed js error

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 document.ready = function() {
-	if (history.replacestate) {
+	if (history.replaceState) {
 		var pageheights = {'/' : 810, '/ueber-uns.html' : 1010,  '/events.html' : 1100,  '/unterstuetzer.html' : 810,  '/kontakt.html' : 900,  '/impressum.html' : 850,  '/libellaswing.html' : 1100,  '/datenschutz.html' : 1250,  '/newsletter.html' : 1000,  '/findusmc.html' : 1100,  '/meissentanzt.html' : 1100,  '/sponsoren.html' : 1000, '/indierocknight.html' : 1100, '/investorrelations.html' : 1050, '/bands.html' : 850, '/anteilseigner.html' : 1020, '/zeitungsartikel.html' : 1100, '/entstehung.html' : 2210};
 		var events = ['/findusmc.html','/libellaswing.html','/indierocknight.html','/meissentanzt.html'];
 
@@ -9,6 +9,7 @@ document.ready = function() {
 			$('#box').height(pageheights[document.location.pathname]);
 			$('a').click(function() {
 				history.pushState(state, "Afrophon", $(this).attr('href'));
+				document.title = "Afrophon";
 				_gaq.push(['_trackPageview', $(this).attr('href')]);
 				var path = "ajax"+$(this).attr('href');
 				if (jQuery.inArray(document.location.pathname, events) != -1)
