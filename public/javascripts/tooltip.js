77f1b77870b2ea3863deeb560cd8d31f67aabd1c
Lucas Waller <lcstwllr@gmail.com>
2011-05-20 18:22:06 +0200

    new last-modified

--- public/javascripts/tooltip.js
+++ public/javascripts/tooltip.js
@@ -3,22 +3,22 @@ $(function() {
 	var masks = $('.mask');
 	var current = $();
 	tooltips.bind('mouseenter', function() {
-		if (current.get(0) != $(this).get(0)) {
+		if (jQuery.inArray($(this).get(0),current.get()) == -1) {
 			$(this).parent().parent().children('.mask').stop().css('z-index', 10).width(90).height(88);
 			$(this).parent().css('z-index', 15).animate({width: $(this).parent().attr('endwidth'), height: $(this).parent().attr('endheight')}, 800, function () {
 
 			});
-			current = $(this);
+			current = current.not($(this).parent().parent().children().children()).add(this);
 		}
 	});
 	tooltips.click(function () {
 		$(this).parent().parent().children('.mask').stop().css('z-index', 10).width(90).height(88);
-		if (current.get(0) != $(this).get(0)) {
+		if (jQuery.inArray($(this).get(0),current.get()) == -1) {
 			$(this).parent().css('z-index', 15).animate({width: $(this).parent().attr('endwidth'), height: $(this).parent().attr('endheight')}, 800, function () {
 
 			});
-			current=$(this);
+			current = current.add(this);
 		} else 
-			current=$();
+			current = current.not(this);
 	});
 });
