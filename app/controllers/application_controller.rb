14eb6f73df845e81081d3564d80b1ab007734cda
Lucas Waller <lcstwllr@gmail.com>
2011-05-21 11:43:33 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   def set_cache
     response.headers["Cache-Control"] = "public, max-age=3153600"
     response.headers["Vary"] = "Accept-Encoding"
-    response.headers["Last-Modified"] = "Fri, 20 Nov 2011 16:00:00 GMT"
+    response.headers["Last-Modified"] = "Sat, 21 Nov 2011 10:00:00 GMT"
   end
 end
