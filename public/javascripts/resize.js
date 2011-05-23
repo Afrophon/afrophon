1ee13c8b1e8942910401f76abbc3bacef134a1d8
Lucas Waller <lcstwllr@gmail.com>
2011-05-23 23:28:21 +0200

    fixed resize: no more header jumping

--- public/javascripts/resize.js
+++ public/javascripts/resize.js
@@ -3,11 +3,13 @@ $(function() {
 	var bc = boxes.children('h4');
 	boxes.bind('click', function() {
 		var box = $(this);
-		box.addClass('active').stop().animate({ height: 224 }, 1000 );
-		box.children().stop().animate({ top: 6 }, 1000 );
-		boxes.not(this).stop().animate({ height: 20 }, 1000 );
-		boxes.not(this).children().stop().animate({ top: -6 }, 1000 , function() {
-			boxes.not(box).removeClass('active');
-		});
+		if (!box.hasClass('active')) {
+			box.addClass('active').stop().animate({ height: 224 }, 1000 ).find('h2').css({ top : -6} );
+			box.children().stop().animate({ top: 6 }, 1000 );
+			boxes.not(this).stop().animate({ height: 20 }, 1000 );
+			boxes.not(this).children().stop().animate({ top: -6 }, 1000 , function() {
+				boxes.not(box).removeClass('active');
+			});
+		}
 	});
 });
