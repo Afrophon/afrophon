bd157b6ba78ae6de1b2b6e8416b615e24d913811
Lucas Waller <lcstwllr@gmail.com>
2011-05-24 17:35:07 +0200

    cache lifetime set to 24h

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_cache
   def set_cache
-    response.headers["Cache-Control"] = "public, max-age=3153600"
+    response.headers["Cache-Control"] = "public, max-age=86400"
     response.headers["Vary"] = "Accept-Encoding"
     response.headers["Last-Modified"] = "Mon, 23 May 2011 13:00:00 GMT"
   end
