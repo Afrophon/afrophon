b2f8a4ca9f2f7df9600c3878129aa5efd631a2b0
Lucas Waller <lcstwllr@gmail.com>
2012-01-23 17:51:00 +0100

    basic html structure

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base    
-  protect_from_forgery
-  before_filter :set_cache
-  def set_cache
-    response.headers["Cache-Control"] = "public, max-age=43200"
-    response.headers["Vary"] = "Accept-Encoding"
-    response.headers["Last-Modified"] = "Sat, 02 June 2011 17:31:00 GMT"
-  end
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Afrophon::Application.config.secret_token = '22a5be3bd65de63fee3f28278eadf9ae591a985aea640438bccf2c24ac4dc16c400f4017e7b4666f2fb8cf8913fe46cd8affcf322de41760b6c2e68c38f8a314'
